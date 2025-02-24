a78a049832483a711592b6f5bae8114338824758
SashaZhukov <sasha01zhukov@gmail.com>
2025-02-24 12:35:29 +0100

    add grt user util and make admin beck architecture

--- laravel/database/migrations/2025_02_24_191137_create_users_table.php
+++ laravel/database/migrations/2025_02_24_191137_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
